56bcbc2f11581d9764a7c254b3db2dd3d38bb91d
Claudio Alberto <contato@claudioalberto.com.br>
2020-03-20 18:29:38 -0300

    Desenvolvimento do Header do Site (Mobile e Desktop)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,15 @@
 $('document').ready(() =>{
 
 });
+let show = true;
+const menuSection = document.querySelector('.navbar-menu-mobile');
+const menuToggle = document.querySelector('.menutoggler');
+
+menuToggle.addEventListener('click', ()=> {
+  
+    document.body.style.overflow = show ? "hidden": "initial";
+    menuToggle.classList.toggle('open', show)
+    menuSection.classList.toggle('on', show)
+    show = !show;
+
+})
